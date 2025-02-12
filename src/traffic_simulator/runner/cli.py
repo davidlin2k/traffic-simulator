c843228f2a253f68200324861c86ace99282cb06
Savanna <savannapan@gmail.com>
2025-02-12 14:48:48 -0500

    mse

--- src/traffic_simulator/runner/cli.py
+++ src/traffic_simulator/runner/cli.py
@@ -8,7 +8,6 @@ from traffic_simulator.ports.link import Link
 from traffic_simulator.ports.strategy import StrategyFactory
 from traffic_simulator.simulator.simulator import Simulator
 
-
 @click.command()
 @click.option(
     "--config",
@@ -47,6 +46,52 @@ def cli(config: str, output: str):
         flow_generator=flow_generator,
         strategy=strategy,
         links=links,
+        link_configs=sim_config.network.links,  # Add link configs
     )
     simulator.run()
     simulator.visualize(save_path=output)
+
+
+# old copy
+# @click.command()
+# @click.option(
+#     "--config",
+#     type=click.Path(exists=True),
+#     default="config.yaml",
+#     help="Path to the YAML configuration file",
+# )
+# @click.option(
+#     "--output",
+#     type=click.Path(),
+#     default="output",
+#     help="Path to the output directory",
+# )
+# def cli(config: str, output: str):
+#     # Create the output directory if it does not exist
+#     pathlib.Path(output).mkdir(parents=True, exist_ok=True)
+
+#     # Load the configuration file
+#     sim_config = load_config(config)
+
+#     flow_size_generator = FlowSizeGeneratorFactory.create_generator(sim_config)
+#     flow_generator = PoissonFlowGenerator(
+#         arrival_rate=sim_config.traffic.flow_arrival.rate,
+#         flow_size_generator=flow_size_generator,
+#     )
+
+#     links = [Link(capacity_bps=link.capacity) for link in sim_config.network.links]
+#     strategy = StrategyFactory.create_strategy(
+#         strategy_name=sim_config.network.strategy,
+#         links=links,
+#         config=sim_config,
+#     )
+
+#     simulator = Simulator(
+#         duration=sim_config.simulation.duration,
+#         flow_generator=flow_generator,
+#         strategy=strategy,
+#         links=links,
+#     )
+#     simulator.run()
+#     simulator.visualize(save_path=output)
+
